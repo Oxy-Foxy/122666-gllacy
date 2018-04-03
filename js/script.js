b80f41e200dd0a804830387a2ef0bc94a1d3b3c0
Oxy-Foxy <oxana.garashcenco@gmail.com>
2018-04-03 14:51:23 +0300

    анимация - сабмит формы при пустых полях

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
-var feedback = document.querySelector(".call-feedback");
 var feedbackModal = document.querySelector(".feedback-wrapper");
+var feedback = document.querySelector(".call-feedback");
 var feedbackClose = document.querySelector(".feedback-close");
+var form = feedbackModal.querySelector(".feedback");
+var name = form.querySelector("#feedback-name");
+var email = form.querySelector("#feedback-email");
+var message = form.querySelector("#feedback-textfield");
+
 
 feedback.addEventListener("click", function(e) {
   e.preventDefault();
-  feedbackModal.classList.add("js-feedback-show");
+  feedbackModal.classList.add("js-feedback-show", "get-big");
+
 });
 
 feedbackClose.addEventListener("click", function(e) {
   e.preventDefault();
   feedbackModal.classList.remove("js-feedback-show");
-})
+  form.classList.remove("modal-error");
+});
+
+
+
+form.addEventListener("submit", function(e) {
+  if(!name.value || !email.value || !message.value) {
+    e.preventDefault();
+    form.classList.add("modal-error");
+  }
+  else {
+    form.classList.remove("modal-error");
+    localStorage.setItem("name", name.value);
+    localStorage.setItem("email", email.value);
+    localStorage.setItem("message", message.value);
+  }
+});
